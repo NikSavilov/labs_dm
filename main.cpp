0a932bee6cf9d192789f4066b2782168e4f3b136
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-17 00:18:19 +0300

    ex5

--- main.cpp
+++ main.cpp
@@ -5,11 +5,27 @@
 using namespace std;
 
 int main() {
-    ifstream input("aplusbb.in");
-    long long first_num, second_num;
-    input >> first_num >> second_num;
-    long long res = first_num + (second_num * second_num);
-    ofstream output("aplusbb.out");
-    output << res;
+    ifstream input("sortland.in");
+    int n;
+    input >> n;
+    double salary[n], id[n];
+    for (int h=0;h<n;h++){
+        input >> salary[h];
+        id[h]=h+1;
+    }
+    for (int i=1;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            if(salary[j]<salary[j+1]){
+                int t = salary[j];
+                salary[j] = salary[j+1];
+                salary[j+1] = t;
+                t = id[j];
+                id[j] = id[j+1];
+                id[j+1] = t;
+            }
+        }
+    }
+    ofstream output("sortland.out");
+    output << id[n-1] << " " << id[n/2] << " " << id[0];
     return 0;
 }
