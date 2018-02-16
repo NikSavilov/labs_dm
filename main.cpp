74bec7cc2c5d3085eaf650dbcd57dc47ae44ee72
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-16 22:39:50 +0300

    2ex

--- main.cpp
+++ main.cpp
@@ -6,20 +6,10 @@ using namespace std;
 
 int main() {
     ifstream input("aplusbb.in");
-    double first_num, second_num;
+    long long first_num, second_num;
     input >> first_num >> second_num;
-    input.close();
-
-    double res = first_num + second_num * second_num;
-
-    FILE * output = fopen("aplusbb.out","w");
-
-    int rounded = res;
-    if (res - rounded == 0){
-        fprintf(output,"%d",rounded);
-    }
-    else
-        fprintf(output,"%f",res);
-
+    long long res = first_num + (second_num * second_num);
+    ofstream output("aplusbb.out");
+    output << res;
     return 0;
 }
