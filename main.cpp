0b27962e151a6d4077961e9580f3b7b078867520
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-16 17:59:07 +0300

    ex3

--- main.cpp
+++ main.cpp
@@ -3,23 +3,31 @@
 #include <iomanip>
 
 using namespace std;
+int move(int i, int j, int i_cur, int j_cur, int sum, int *max_sum, int **table){
 
+}
 int main() {
-    ifstream input("aplusbb.in");
-    double first_num, second_num;
-    input >> first_num >> second_num;
-    input.close();
-
-    double res = first_num + second_num * second_num;
-
-    FILE * output = fopen("aplusbb.out","w");
-
-    int rounded = res;
-    if (res - rounded == 0){
-        fprintf(output,"%d",rounded);
+    int i,j;
+    ifstream input("turtle.in");
+    input >> i >> j;
+    int **table = new int*[i+1];
+    for (int oi = 0; oi < i + 1; oi++) {
+        table[oi] = new int[j + 1];
+        for (int k = 0; k < j + 1; k++){
+            table[oi][k] = 0;
+        }
     }
-    else
-        fprintf(output,"%f",res);
-
+    for (int oi = i; oi > 0; oi--){
+        for (int oj = 1; oj <= j; oj++){
+            input >> table[oi][oj];
+        }
+    }
+    for (int oi = 1; oi <= i; oi++){
+        for (int oj = 1; oj <= j; oj++){
+            table[oi][oj] += (table[oi-1][oj] > table[oi][oj-1] ? table[oi-1][oj] : table[oi][oj-1]);
+        }
+    }
+    ofstream output("turtle.out");
+    output << table[i][j];
     return 0;
 }
