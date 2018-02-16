9a6206efe5d2972ef9b73b0305af83c26d49e7fd
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-16 22:31:00 +0300

    ex4

--- main.cpp
+++ main.cpp
@@ -1,33 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-
+#include <vector>
 using namespace std;
-int move(int i, int j, int i_cur, int j_cur, int sum, int *max_sum, int **table){
-
-}
 int main() {
-    int i,j;
-    ifstream input("turtle.in");
-    input >> i >> j;
-    int **table = new int*[i+1];
-    for (int oi = 0; oi < i + 1; oi++) {
-        table[oi] = new int[j + 1];
-        for (int k = 0; k < j + 1; k++){
-            table[oi][k] = 0;
+    ifstream input("smallsort.in");
+    int n;
+    input >> n;
+    int arr[n];
+    for (int i=0;i < n;i++){
+        input >> arr[i];
+        for (int j = i - 1; j >= 0; j--){
+            if (arr[j+1] < arr[j]){
+                int t = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = t;
+            }
         }
     }
-    for (int oi = i; oi > 0; oi--){
-        for (int oj = 1; oj <= j; oj++){
-            input >> table[oi][oj];
-        }
+    ofstream output("smallsort.out");
+    for (int h=0; h < n; h++){
+        output << arr[h] << " ";
     }
-    for (int oi = 1; oi <= i; oi++){
-        for (int oj = 1; oj <= j; oj++){
-            table[oi][oj] += (table[oi-1][oj] > table[oi][oj-1] ? table[oi-1][oj] : table[oi][oj-1]);
-        }
-    }
-    ofstream output("turtle.out");
-    output << table[i][j];
     return 0;
 }
