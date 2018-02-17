d5f985cc5349bd9d229fbe10478e0a95aeead022
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-17 15:04:52 +0300

    ex1

--- main.cpp
+++ main.cpp
@@ -4,19 +4,12 @@ using namespace std;
 
 int main() {
     ifstream input("aplusb.in");
-    if (!input.is_open()){
-        exit(1);
-    }
     long int first_num, second_num;
     input >> first_num >> second_num;
     input.close();
-
     long sum = first_num + second_num;
-
     ofstream output("aplusb.out");
-    if (!output.is_open()){
-        exit(2);
-    }
     output << sum;
+    output.close();
     return 0;
 }
