66db622925132e20841e933039ca9124eaf69ef7
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-17 15:48:17 +0300

    ex5

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main() {
     int n;
     input >> n;
     double salary[n], id[n];
+    // Filling two related arrays
     for (int h=0;h<n;h++){
         input >> salary[h];
         id[h]=h+1;
@@ -16,16 +17,18 @@ int main() {
     for (int i=1;i<n;i++){
         for(int j=0;j<n-i;j++){
             if(salary[j]<salary[j+1]){
-                int t = salary[j];
-                salary[j] = salary[j+1];
-                salary[j+1] = t;
-                t = id[j];
-                id[j] = id[j+1];
-                id[j+1] = t;
+                salary[j] += salary[j+1];
+                salary[j+1] = salary[j] - salary[j+1];
+                salary[j] = salary[j] - salary[j+1];
+                id[j] += id[j+1];
+                id[j+1] = id[j] - id[j+1];
+                id[j] = id[j] - id[j+1];
             }
         }
     }
+    input.close();
     ofstream output("sortland.out");
     output << id[n-1] << " " << id[n/2] << " " << id[0];
+    output.close();
     return 0;
 }
