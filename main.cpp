14179a3c92edc1726a3535397aff704060a5185c
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-17 16:40:22 +0300

    qs

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ int main() {
     for (int i=1;i<n;i++){
         for(int j=0;j<n-i;j++){
             if(salary[j]<salary[j+1]){
+                //Swapping
                 salary[j] += salary[j+1];
                 salary[j+1] = salary[j] - salary[j+1];
                 salary[j] = salary[j] - salary[j+1];
