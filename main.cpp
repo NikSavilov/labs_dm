7e48b87709ec870db3c87702fd84d70649033fc6
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-17 15:35:14 +0300

    ex4

--- main.cpp
+++ main.cpp
@@ -8,19 +8,22 @@ int main() {
     int n;
     input >> n;
     int arr[n];
+    // Pushback at end, from previous to first swapping while num is less then neighbour
     for (int i=0;i < n;i++){
         input >> arr[i];
         for (int j = i - 1; j >= 0; j--){
             if (arr[j+1] < arr[j]){
-                int t = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = t;
+                arr[j+1] += arr[j];
+                arr[j] = arr[j+1] - arr[j];
+                arr[j+1] = arr[j+1] - arr[j];
             }
         }
     }
+    input.close();
     ofstream output("smallsort.out");
     for (int h=0; h < n; h++){
         output << arr[h] << " ";
     }
+    output.close();
     return 0;
 }
