706f72e675b632bfe9169b794357e3830b7dbcc2
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-17 15:16:22 +0300

    ex3

--- main.cpp
+++ main.cpp
@@ -3,13 +3,11 @@
 #include <iomanip>
 
 using namespace std;
-int move(int i, int j, int i_cur, int j_cur, int sum, int *max_sum, int **table){
-
-}
 int main() {
     int i,j;
     ifstream input("turtle.in");
     input >> i >> j;
+    // Creating table with borders and filling it with nulls
     int **table = new int*[i+1];
     for (int oi = 0; oi < i + 1; oi++) {
         table[oi] = new int[j + 1];
@@ -17,11 +15,14 @@ int main() {
             table[oi][k] = 0;
         }
     }
+    // Reading inverted table
     for (int oi = i; oi > 0; oi--){
         for (int oj = 1; oj <= j; oj++){
             input >> table[oi][oj];
         }
     }
+    input.close();
+    // Counting the most expensive way from neighbours
     for (int oi = 1; oi <= i; oi++){
         for (int oj = 1; oj <= j; oj++){
             table[oi][oj] += (table[oi-1][oj] > table[oi][oj-1] ? table[oi-1][oj] : table[oi][oj-1]);
@@ -29,5 +30,7 @@ int main() {
     }
     ofstream output("turtle.out");
     output << table[i][j];
+    output.close();
+    delete table;
     return 0;
 }
