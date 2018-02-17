09cd68f698123ad03a8dc59c04bafd77859e95f1
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-17 15:06:43 +0300

    ex2

--- main.cpp
+++ main.cpp
@@ -8,8 +8,10 @@ int main() {
     ifstream input("aplusbb.in");
     long long first_num, second_num;
     input >> first_num >> second_num;
+    input.close();
     long long res = first_num + (second_num * second_num);
     ofstream output("aplusbb.out");
     output << res;
+    output.close();
     return 0;
 }
