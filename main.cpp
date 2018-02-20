3012ebf51a507ae95a884b2eef339592578b3dde
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-20 09:19:06 +0300

    2ex2

--- main.cpp
+++ main.cpp
@@ -3,20 +3,25 @@
 #include <iomanip>
 
 using namespace std;
-void quicksort(int *arr, int first, int last)
+struct Runner {
+    int number;
+    string name, country;
+};
+void quicksort(Runner *arr, int first, int last)
 {
-    int mid, count;
+    int mid;
+    Runner tmp;
     int f=first, l=last;
-    mid=arr[(f+l) / 2]; //вычисление опорного элемента
+    mid = arr[(f+l) / 2].number; //вычисление опорного элемента
     do
     {
-        while (arr[f]<mid) f++;
-        while (arr[l]>mid) l--;
+        while (arr[f].number<mid) f++;
+        while (arr[l].number>mid) l--;
         if (f<=l) //перестановка элементов
         {
-            count=arr[f];
-            arr[f]=arr[l];
-            arr[l]=count;
+            tmp = arr[f];
+            arr[f] = arr[l];
+            arr[l] = tmp;
             f++;
             l--;
         }
@@ -25,17 +30,36 @@ void quicksort(int *arr, int first, int last)
     if (first<l) quicksort(arr, first, l);
     if (f<last) quicksort(arr, f, last);
 }
+
 int main() {
-    ifstream input("smallsort.in");
+    ifstream input("race.in");
     int n;
     input >> n;
-    int arr[n];
-    for (int i=0;i<n;i++){
-        input >> arr[i];
+    Runner arr[n];
+    for (int i=1; i < n+1; i++){
+        input >> arr[i].country >> arr[i].name;
+        arr[i].number = i;
     }
-    quicksort(arr, 0, n - 1);
+    int count = n;
+    string cur_country = arr[1].country, next_country = cur_country;
+    while (count !=0) {
+        cout << "=== " + cur_country + " ==="<< endl;
+        for (int i = 1; i < n+1; i++){
+            if (arr[i].country != ""){
+                if (arr[i].country == cur_country){
+                    cout << arr[i].name << endl;
+                    count--;
+                    arr[i].country = "";
+                }
+                else if (next_country == cur_country){
+                    next_country = arr[i].country;
+                }
+            }
+        }
+        cur_country = next_country;
+    }
     input.close();
-    ofstream output("smallsort.out");
+    ofstream output("race.out");
     output.close();
     return 0;
 }
