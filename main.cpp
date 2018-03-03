6fcd2a133676d73a4bc87abae36462a77457fb81
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-03-03 14:47:58 +0300

    2ex2

--- main.cpp
+++ main.cpp
@@ -4,11 +4,32 @@
 
 using namespace std;
 struct Runner {
-    int number;
     string name, country;
+    int number;
 };
-void quicksort(Runner *arr, int first, int last)
-{
+void quicksort_a(Runner *arr, int first, int last) {
+    string mid;
+    Runner tmp;
+    int f=first, l=last;
+    mid = arr[(f+l) / 2].country; //вычисление опорного элемента
+    do
+    {
+        while (arr[f].country<mid) f++;
+        while (arr[l].country>mid) l--;
+        if (f<=l) //перестановка элементов
+        {
+            tmp = arr[f];
+            arr[f] = arr[l];
+            arr[l] = tmp;
+            f++;
+            l--;
+        }
+    } while (f <= l);
+
+    if (first<l) quicksort_a(arr, first, l);
+    if (f<last) quicksort_a(arr, f, last);
+}
+void quicksort(Runner *arr, int first, int last) {
     int mid;
     Runner tmp;
     int f=first, l=last;
@@ -30,34 +51,39 @@ void quicksort(Runner *arr, int first, int last)
     if (first<l) quicksort(arr, first, l);
     if (f<last) quicksort(arr, f, last);
 }
-
 int main() {
     ifstream input("race.in");
-    int n;
+    int n, number = 0;
     input >> n;
     Runner arr[n];
-    for (int i=1; i < n+1; i++){
+    for (int i=0; i < n; i++){
         input >> arr[i].country >> arr[i].name;
-        arr[i].number = i;
+        arr[i].number = number;
+        number++;
     }
-    int count = n;
-    ofstream output("race.out");
-    string cur_country = arr[1].country, next_country = cur_country;
-    while (count !=0) {
-        output << "=== " + cur_country + " ==="<< endl;
-        for (int i = 1; i < n+1; i++){
-            if (arr[i].country != ""){
-                if (arr[i].country == cur_country){
-                    output << arr[i].name << endl;
-                    count--;
-                    arr[i].country = "";
-                }
-                else if (next_country == cur_country){
-                    next_country = arr[i].country;
-                }
-            }
+    quicksort_a(arr,0,n-1);
+    string current_country = arr[0].country, next_country = " ";
+    int st=0,en=0;
+    for (int h=0;;){
+        while (arr[en+1].country == current_country) {
+            en++;
         }
-        cur_country = next_country;
+        quicksort(arr,st,en);
+        st = en + 1;
+        en++;
+        if(en==n){
+            break;
+        }
+        current_country = arr[en].country;
+    }
+    ofstream output("race.out");
+    for (int i=0; i < n; i++){
+        current_country = arr[i].country;
+        if (next_country != current_country){
+            output << "=== " << current_country << " ===" << endl;
+            next_country = current_country;
+        }
+        output << arr[i].name << endl;
     }
     input.close();
     output.close();
