dd39fd195617749fe88142346b4809df74a405da
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-03-05 14:40:36 +0300

    2ex4

--- main.cpp
+++ main.cpp
@@ -1,50 +1,27 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
-#define VEC vector
-#define PB push_back
+#include <cmath>
 
 using namespace std;
 
-void mergeSort(VEC <int> &a, int L, int R, long long &ans) {
-    if (R - L < 2) return;
-    mergeSort(a, L, L + (R - L) / 2, ans);
-    mergeSort(a, L + (R - L) / 2, R, ans);
-    VEC <int> tmp;
-    int Lf = L, Rf = L + (R - L) / 2;
-    int Ls = Rf, Rs = R;
-    while (tmp.size() < R - L) {
-        if (Lf >= Rf || (a[Ls] < a[Lf] && Ls < R)) {
-            tmp.PB(a[Ls]);
-            Ls++;
-            ans += L + (R - L) / 2 - Lf;
-        } else {
-            tmp.PB(a[Lf]);
-            Lf++;
-        }
-    }
-    for (int i = L; i < R; ++i) {
-        a[i] = tmp[i - L];
-    }
-}
-
 int main() {
-    int n;
-    long long ans = 0;
-
-    ifstream cin("inversions.in");
-    ofstream cout("inversions.out");
-
+    ifstream cin("antiqs.in");
+    ofstream cout("antiqs.out");
+    long n;
     cin >> n;
-    if (n == 0 || n == 1) {
-        cout << 0;
-        return 0;
+    long mas[n];
+    for (int i=1;i<=n;i++){
+        
     }
-    VEC <int> a(n);
-    for (int i = 0; i < n; ++i)
-        cin >> a[i];
-    mergeSort(a, 0, a.size(), ans);
-    cout << ans;
+    long pivot = (n - 1) / 2, num = n;
+    for (long i=0; i<n; i++){
+        mas[pivot] = num;
+        pivot += pow(-1,i) * (i+1);
+        num--;
+    }
+    for (long i =0;i<n;i++){
+        cout << mas[i] << " ";
+    }
     return 0;
 }
