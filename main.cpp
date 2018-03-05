aa126b2bd20d0bd279af094e00218635b27d1dfe
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-03-05 16:17:25 +0300

    2ex4

--- main.cpp
+++ main.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 int main() {
     ifstream cin("antiqs.in");
     ofstream cout("antiqs.out");
-    long n;
-    cin >> n;
-    long mas[n];
-    for (int i=1;i<=n;i++){
-        
+    int n, m, A, B, C;
+    cin >> n >> m >> A >> B >> C;
+    vector <int> myvector(n);
+    cin >> myvector[0];
+    cin >> myvector[1];
+    for (int i=2;i<n;i++){
+        int tmp = A * myvector[i-2] + B * myvector[i-1] + C;
+        myvector.push_back(tmp);
     }
-    long pivot = (n - 1) / 2, num = n;
-    for (long i=0; i<n; i++){
-        mas[pivot] = num;
-        pivot += pow(-1,i) * (i+1);
-        num--;
-    }
-    for (long i =0;i<n;i++){
-        cout << mas[i] << " ";
-    }
+    nth_element(myvector.begin(), myvector.begin() + m, myvector.end());
+
     return 0;
 }
