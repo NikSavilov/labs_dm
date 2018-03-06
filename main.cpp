b75e17f381d48f0e68de62f805c8dec60f867951
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-03-06 10:15:17 +0300

    2ex4

--- main.cpp
+++ main.cpp
@@ -6,18 +6,19 @@
 using namespace std;
 
 int main() {
-    ifstream cin("antiqs.in");
-    ofstream cout("antiqs.out");
-    int n, m, A, B, C;
-    cin >> n >> m >> A >> B >> C;
-    vector <int> myvector(n);
-    cin >> myvector[0];
-    cin >> myvector[1];
-    for (int i=2;i<n;i++){
-        int tmp = A * myvector[i-2] + B * myvector[i-1] + C;
-        myvector.push_back(tmp);
+    ifstream input("kth.in");
+    int n, k, A, B, C;
+    input >> n >> k;
+    input >> A >> B >> C;
+    vector<int> myVector(10);
+    input >> myVector[0];
+    input >> myVector[1];
+    for (int h = 2; h < n; h++){
+        myVector[h] = A * myVector[h-2] + B * myVector[h-1] + C;
     }
-    nth_element(myvector.begin(), myvector.begin() + m, myvector.end());
-
+    nth_element (myVector.begin(), myVector.begin() + k - 1, myVector.end());
+    for (int h = 0; h < n; h++){
+        cout << myVector[h] << " ";
+    }
     return 0;
 }
