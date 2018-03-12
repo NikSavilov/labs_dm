4723ce8bea5d2ac002097c47ae7c403389d1abeb
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-03-12 16:29:39 +0300

    3ex1

--- main.cpp
+++ main.cpp
@@ -1,57 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
-const int MAXN = 1000 + 15, MAXVAL = 255;
-
-int n, m, k;
-char s[MAXN][MAXN], B[MAXN][MAXN];
-int C[MAXVAL];
-
-void RadixSort() {
-    for (int i = m - 1; i > m - k - 1; --i) {
-        for (int j = 0; j < MAXVAL; ++j)
-            C[j] = 0;
-        for (int j = 0; j < n; ++j) {
-            int d = (s[j][i] - '0');
-            C[d]++;
-        }
-        int cnt = 0;
-        for (int j = 0; j < MAXVAL; ++j) {
-            int tmp = C[j];
-            C[j] = cnt;
-            cnt += tmp;
-        }
-        for (int j = 0; j < n; ++j) {
-            int d = int(s[j][i] - '0');
-            for (int g = 0; g < m; ++g) {
-                B[C[d]][g] = s[j][g];
-            }
-            C[d]++;
-        }
-        for (int j = 0; j < n; ++j)
-            for (int g = 0; g < m; ++g)
-                s[j][g] = B[j][g];
-    }
-}
-
 int main() {
-    ifstream cin("radixsort.in");
-    ofstream cout("radixsort.out");
-
-    cin >> n  >> m >> k;
-
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < m; ++j)
-            cin >> s[i][j];
-
-    RadixSort();
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j)
-            cout << s[i][j];
-        cout << "\n";
+    ifstream cin("stack.in");
+    ofstream cout("stack.out");
+    int n;
+    cin >> n;
+    vector <int> stack;
+    string z;
+    int m;
+    for (int i = 0; i < n; ++i){
+        cin >> z;
+        if (z == "+"){
+            cin >> m;
+            stack.push_back(m);
+        }
+        else if (z == "-"){
+            cout << stack[stack.size()-1] << endl;
+            stack.pop_back();
+        }
     }
 
 
