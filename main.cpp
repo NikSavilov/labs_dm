05c6e354da39d30277f5e6f95d123997caa7f220
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-03-16 18:16:25 +0300

    3ex5

--- main.cpp
+++ main.cpp
@@ -1,36 +1,23 @@
 #include <fstream>
-#include <stack>
 using namespace std;
 
-int make(int a, int b, char c){
-    switch (c){
-        case '+': return a+b;
-        case '-': return a-b;
-        case '*': return a*b;
-        default: return 0;
-    }
-}
-
 int main() {
-    ifstream cin("postfix.in");
-    ofstream cout("postfix.out");
-    int a,b;
-    char c;
-    stack <int> s;
-    while(cin >> c){
-        if (c!=' '){
-            if (c >= '0' and c<='9'){
-                s.push(c - '0');
-            }
-            else {
-                a = s.top();
-                s.pop();
-                b = s.top();
-                s.pop();
-                s.push(make(b,a,c));
-            }
+    ifstream cin("isheap.in");
+    ofstream cout("isheap.out");
+    int n, i = 0, flag = 0;
+    cin >> n;
+    int a[n];
+    for (int o = 0; o < n; o++)
+        cin >> a[o];
+    while (i * 2 + 2 < n) {
+        if (a[i] > a[i * 2 + 1] || a[i] > a[(i * 2 + 2 < n) ? i * 2 + 2: i * 2 + 1]) {
+            flag = 1;
+            break;
         }
+        i++;
     }
-    cout << s.top();
+
+    if (flag == 1) cout << "NO";
+    else cout << "yes";
     return 0;
 }
